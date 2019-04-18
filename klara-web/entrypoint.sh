5b298e7a5538613842a07ad4185b944a36f65f65
Jose Palanco <jpalanco@gmail.com>
2019-04-18 21:20:15 +0000

    Added agent

--- klara-web/entrypoint.sh
+++ klara-web/entrypoint.sh
@@ -6,6 +6,7 @@ if [ ! -f /tmp/configured ]; then
    sleep 15
    # FIXME: some commands this should be done in Dockerfile
    cd /home/projects/klara-github-repo
+   echo "INSERT INTO `agents` (`id`, `description`, `auth`) VALUES (1, 'test', 'test');" >> ./install/db_patches/db_schema.sql
    mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -D$MYSQL_DATABASE -h klara-db < ./install/db_patches/db_schema.sql && touch /tmp/configured && echo 'Finished';
 else
   echo 'Already configured';
